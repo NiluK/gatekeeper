45aa7258a6497224cc98f0226ac21e5289810d6a
Ian Newmarch <ian.newmarch@ngr.com.au>
2022-03-18 20:41:03 +1100

    added hardhat / basic contract

---
+++ contracts/hardhat.config.ts
@@ -0,0 +1,37 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "hardhat-gas-reporter";
+import "@typechain/hardhat";
+//https://github.com/nomiclabs/hardhat/tree/master/packages/hardhat-ethers
+//https://hardhat.org/guides/waffle-testing.html
+import "@nomiclabs/hardhat-ethers";
+
+import { config } from "./scripts/config";
+
+const hardhatConfig: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    rinkeby: {
+      url: config.rinkeby.url,
+      accounts: [config.privateKey],
+    },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    coinmarketcap: config.coinmarketcap,
+  },
+  mocha: {
+    timeout: 120000,
+  },
+  solidity: {
+    version: "0.8.11",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 20000,
+      },
+    },
+  },
+};
+
+export default hardhatConfig;
