03013523689ab1107ec1b45184c2fc58701e8a56
Ian Newmarch <ian.newmarch@ngr.com.au>
2022-03-18 22:17:29 +1100

    nft address

--- contracts/contracts/Gatekeeper.sol
+++ contracts/contracts/Gatekeeper.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0 <0.9.0;
 contract Gatekeeper {
     constructor() {}
 
-    // address nftAddress = "0x";
+    address nftAddress = "0xD90855960eF8276C6361C85EaaEFEa501Ab30c72";
 
     function allowed(bytes calldata eventData) public view returns (bool) {
         address from = msg.sender;
